df0ddcf162aab63d1e4e39acf25e901f965893fb
gabrielcomez <gabri.jci@gmail.com>
2020-04-23 15:49:58 +0200

    basic user profile page working + added default profile picture for users

--- src/Users/Profile/UserProfile.js
+++ src/Users/Profile/UserProfile.js
@@ -1,5 +1,37 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import defProfile from "../../defProfile.jpeg";
 
-export default function UserProfile() {
-  return <main>user data will be here</main>;
+export default function UserProfile(props) {
+  console.log(props);
+
+  return (
+    <div>
+      {props.user.token === null ? (
+        <div>
+          If you alredy have an account,
+          <Link to="/signin"> sing in</Link>
+          🔑
+          <br />
+          If you don't,
+          <Link to="/signup"> sing up</Link>
+          🙌
+        </div>
+      ) : (
+        <div>
+          <h1>Welcome back {props.user.name}</h1>
+          {props.user.image ? (
+            <img src={props.user.image} alt="profile pic" />
+          ) : (
+            <img
+              src={defProfile}
+              alt="default profile pic"
+              height="auto"
+              width="500"
+            />
+          )}
+        </div>
+      )}
+    </div>
+  );
 }
--- src/Users/Profile/index.js
+++ src/Users/Profile/index.js
@@ -1,8 +1,18 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
 import UserProfile from "./UserProfile";
 
-export default class UserProfileContainer extends Component {
+class UserProfileContainer extends Component {
   render() {
-    return <UserProfile />;
+    return <UserProfile user={this.props.user} />;
   }
 }
+
+const mapStateToProps = (state) => {
+  console.log(">>>state @profile comp", state);
+  return {
+    user: state.user,
+  };
+};
+
+export default connect(mapStateToProps)(UserProfileContainer);
